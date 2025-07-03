8a92e5fa89f44c23666927e850c1219fca9fb68a
Vitor Alves <alvesbazeth881@gmail.com>
2025-07-03 19:47:49 -0300

    Update ex76.py

--- ex76.py
+++ ex76.py
@@ -1,18 +1,15 @@
-print('--' * 20)
-print('Listagem De Preços')
-print('--' * 20)
 listagem = ('Lápis', 1.75, 'Borracha', 2.00, 'Caderno', 15.90, 'Estojo', 25.00, 'Transferidor', 4.20, 'Compasso', 9.99, 'Mochila', 120.32, 'Canetas', 22.30, 'Livro', 34.90)
 
-for c in range(0, listagem[10]):
-    print(c)
+for pos in range(0,len(listagem)):
+    if pos % 2 == 0:
+        print(f'{listagem[pos]:.<30}', end='')
+    else:
+        print(f'R${listagem[pos]:>7.2f}')
 
+# 2f - Para ter duas casas decimais
 
+# :>7 - Joga o Elemento pra direita
 
+#:<30 - Joga o elemento pra esquerda
 
-
-
-
-
-
-#for pos, contador in enumerate(listagem):
-    #print(f'{contador}  '  f'{pos}')
+#:^30 - Centraliza o Elemento
