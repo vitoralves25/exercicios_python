dc2aff1a6ebb3ba118a80cff1c599c8ca5570e07
Vitor Alves <alvesbazeth881@gmail.com>
2025-07-03 19:09:31 -0300

    Atualizando textos

--- ex74.py
+++ ex74.py
@@ -1,4 +1,7 @@
-# Meu jeito
+# \n pula para a linha de baixo
+
+#Um jeito
+
 from random import randint
 num_aleatorio = tuple(randint(0,10) for c in range(0,5))
 print(f'Os números sorteados foram {num_aleatorio}')
@@ -11,7 +14,18 @@ menor_numero = min(num_aleatorio)
 print(f'O maior número sorteado foi {maior_numero}')
 print(f'O menor número sorteado foi {menor_numero}')
 
-#Jeito do Guanabara
+# Outro Jeito
+
+
+num_aleatorio = (randint(0,10), randint(0,10), randint(0,10), randint(0,10), randint(0,10))
+# Para exibir os valores em forma organizada é só fazer com for
+
+for num in num_aleatorio:
+    print(f'{num} ', end=' ')
+print(f'\nO maior valor sorteado foi {max(num_aleatorio)}')
+print(f'O menor valor sorteado foi {min(num_aleatorio)}')
+
+
 
 
 
--- ex75.py
+++ ex75.py
@@ -1,30 +1,51 @@
-cont = 0
-par = 2
+# Primeiro jeito de se fazer
+
+#cont = 0
+#par = 2
 from random import randint
-num = int(input('Digite um número: '))
-segundo_num = int(input('Digite um segundo número: '))
-terceiro_num = int(input('Digite um terceiro número: '))
-quarto_num = int(input('Digite o quarto número: '))
+#num = int(input('Digite um número: '))
+#segundo_num = int(input('Digite um segundo número: '))
+#terceiro_num = int(input('Digite um terceiro número: '))
+#quarto_num = int(input('Digite o quarto número: '))
 
-tupla = (num, segundo_num, terceiro_num, quarto_num)
-print(f'Você digitou esses números {tupla}')
-#   for pos in tupla:
-if 9 in tupla:
-    cont += 1
-    print(f'O Número 9 foi exibido {cont} vez')
-elif 9 not in tupla:
-    print('O número 9 não foi exibido na listagem')
-if 3 in tupla:
-    resultado = tupla.index(3)
-    print(f'O 3 está na posição {resultado}* posição do índice')
-elif 3 not in tupla:
-    print('Não tem nenhum valor 3 na lista')
+#tupla = (num, segundo_num, terceiro_num, quarto_num)
+#print(f'Você digitou esses números {tupla}')
+#if 9 in tupla:
+#    cont += 1
+#    print(f'O Número 9 foi exibido {cont} vez/vezes')
+#elif 9 not in tupla:
+#    print('O número 9 não foi exibido na listagem')
+#if 3 in tupla:
+#    resultado = tupla.index(3)
+#    print(f'O 3 está na posição {resultado}* posição do índice')
+#elif 3 not in tupla:
+#    print('Não tem nenhum valor 3 na lista')
 
-for pos in tupla:
-    if pos % 2 == 0:
-        print(f'O número {pos} é par')
-    else:
-        print(f'O número {pos} é impar')   
+#print('Foram inseridos os valores pares', end=' ')
+#for pos in tupla:
+#    if pos % 2 == 0:
+#       print(f'{pos}', end=', ')
+
+
+# Segundo jeito de se fazer
+
+
+# Para transformar input em uma tupla, basta fazer esse comando abaixo de parênteses antes do int e depois do último parênteses
+
+num = (int(input('Digite seu Número: ')), int(input('Digite seu Número: ')), int(input('Digite seu Número: ')), int(input('Digite seu Número: ')))
+print(num)
+print(f'O valor 9 apareceu {num.count(9)} vezes')
+
+if 3 in num:
+    print(f'O valor 3 foi digitado na {num.index(3)+1 }* posição')
+else:
+    print('Número 3 não foi inserido na lista')
+
+print('Temos os valores pares', end=' ')
+for n in num:
+    if n % 2 == 0:
+        print(f'{n}', end=' ')
+
 
 
 
