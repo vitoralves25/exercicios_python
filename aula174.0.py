51728740b8fcbaca297a790f5be3bed057cff7fb
Vitor Alves <alvesbazeth881@gmail.com>
2025-07-08 14:53:57 -0300

    Atualizando textos

---
+++ aula174.0.py
@@ -0,0 +1,6 @@
+numeros = [2,3,4,5]
+num = numeros[:]
+numeros[2] = 9
+print(f'Lista de Números: {numeros}')
+print(f'Lista de num: {num}')
+
--- ex78.py
+++ ex78.py
@@ -1,34 +1,7 @@
-cont = 0
-valores = []
-maior = valores
-menor = valores
-for c in range(0,5):
-    valores.append(int(input(f'Digite um valor para a Posição {c}: ')))
-print('>=' * 18)
-print(f'Você digitou os valores {valores}')
+numeros = []
+for cont in range(0,5):
+    numeros.append(int(input(f'Digite seu Número na Posição {cont}: ')))
 
-for pos, v in enumerate(valores, ):
-    if pos in valores:
-        maior = max(valores)
-    if pos in valores:
-        menor = min(valores)
-    if pos in valores:
-        pos = v
-print(f'O maior valor {maior} está na posição {pos}')
-print(f'O menor valor {menor} está na posição {pos}')
-
-    
-#for v in valores:
-    #print(f'Os valores são {v}')
-
-        
-
-
-
-
-        #print(f'O maior valor foi {max(valores)} na posição {pos}')
-    #if min(valores) in valores:
-        #print(f'O menor valor foi {min(valores)} na posição {pos}')
-
-
-#print(f'O maior valor foi {max(valores)} na posição {pos} \nO menor valor foi {min(valores)} na posição {pos}')
+print(f'Os valores digitados foram {numeros}')
+print(f'O maior valor digitado foi {max(numeros)} que está na Posição {numeros.index(max(numeros))}')
+print(f'O menor valor digitado foi {min(numeros)} que está na Posição {numeros.index(min(numeros))} ')
