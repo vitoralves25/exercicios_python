65029cf9b1423c544bd81ff45b629eeb80412f6b
Vitor Alves <alvesbazeth881@gmail.com>
2025-07-09 18:21:19 -0300

    Atualizando Textos

--- ex78.py
+++ ex78.py
@@ -1,7 +1,25 @@
 numeros = []
+maior = menor =  0
 for cont in range(0,5):
     numeros.append(int(input(f'Digite seu Número na Posição {cont}: ')))
 
+    if cont == 0:
+        maior = menor = numeros[cont]
+    else:
+        if numeros[cont] > maior:
+            maior = numeros[cont]
+        if numeros[cont] < menor:
+            menor = numeros[cont]
+
 print(f'Os valores digitados foram {numeros}')
-print(f'O maior valor digitado foi {max(numeros)} que está na Posição {numeros.index(max(numeros))}')
-print(f'O menor valor digitado foi {min(numeros)} que está na Posição {numeros.index(min(numeros))} ')
+print(f'O maior valor digitado foi {maior} nas posições ', end='')
+for i, v in enumerate(numeros):
+    if maior == v:
+        print(f'{i}...', end='')
+
+
+
+print(f'\nO menor valor digitado foi {menor} que está nas posições ', end='')
+for i, v in enumerate(numeros):
+    if menor == v:
+        print(f'{i}...', end='')
---
+++ ex83.py
@@ -0,0 +1,5 @@
+expresao = str(input('Digite uma expressão: ')).strip()
+for ex in expresao:
+    print(ex)
+for ex :
+    print(ex)
