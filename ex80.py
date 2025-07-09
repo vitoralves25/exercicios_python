1ebe80f9364883b6347be7c9535cd612084dd657
Vitor Alves <alvesbazeth881@gmail.com>
2025-07-09 00:39:50 -0300

    Atualizando Textos

---
+++ ex80.py
@@ -0,0 +1,13 @@
+valores = []
+for c in range(0,5):
+    valores.append(int(input('Digite seu Número: ')))
+    print(c)
+
+    posicao = 0
+    for num in valores:
+        print(num)
+print(f'{valores}')
+
+
+  
+    
