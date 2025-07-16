e7f9a1d612c324fc28ae89264d6dff4192228918
Vitor Alves <alvesbazeth881@gmail.com>
2025-07-16 09:53:55 -0300

    Update aula17_parte2.py

--- aula17_parte2.py
+++ aula17_parte2.py
@@ -1,12 +1,12 @@
-#teste = []
-#teste.append('Vitor')
-#teste.append('Alves')
-#galera = []
-#galera.append(teste[:])
-#teste[0] = 'Digney'
-#teste[1] = 'Bast'
-#galera.append(teste[:])
-#print(galera)
+teste = []
+teste.append('Vitor')
+teste.append('Alves')
+galera = []
+galera.append(teste[:])
+teste[0] = 'Raynara'
+teste[1] = 'Silva'
+galera.append(teste[:])
+print(galera)
 
 # Ou posso fazer a declaração de galera de outra maneira, Colocando colchetes dentro de colchetes, ou seja, lista dentro de lista
 
@@ -20,23 +20,23 @@
     #print(f'{p[0]} tem {p[1]} de idade')
 
 
-galera = []
-idade = []
-totmaior = totmenor = 0
-for c in range(0,5):
-    galera.append(str(input('nome: ')))
-    idade.append(int(input('idade: ')))
-    galera.append(idade[:])
-    idade.clear()
-print(galera)
+#galera = []
+#idade = []
+#totmaior = totmenor = 0
+#for c in range(0,5):
+#    galera.append(str(input('nome: ')))
+#    idade.append(int(input('idade: ')))
+#    galera.append(idade[:])
+#    idade.clear()
+#print(galera)
 
-for p in galera:
-    if p[1] >= 21:
-        print(f'{p[0]}')
-        totmaior += 1
-    if p[1] <= 21:
-        print(f'{p[0]}')
-        totmenor += 1
+#for p in galera:
+#    if p[1] >= 21:
+#        print(f'{p[0]}')
+#        totmaior += 1
+#    if p[1] <= 21:
+#        print(f'{p[0]}')
+#        totmenor += 1
 
-print(galera)
+#print(galera)
 
